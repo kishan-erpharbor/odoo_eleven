770392e0d0bb05258883408149a6d8160ccaac79
kishan-erpharbor <kishan.b.erpharbor@gmail.com>
2019-08-06 16:05:04 +0530

    [Add] Add Module

---
+++ hospital/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Hospital Management System',
+    'description': 'Hospital Management System',
+    'author': 'Samarpan Trust',
+    'website': 'www.samarpan.org',
+    'version': "11.0.1.0.0",
+    'depends': ['base'],
+    'data': [
+        'wizard/charge_wiz_view.xml',
+        'views/hospital_view.xml',
+        'views/doctor_view.xml',
+        'views/patient_view.xml',
+        'views/shift_view.xml',
+        'views/ward_view.xml',
+        'views/department_view.xml',
+        'views/payment_view.xml',
+        'views/registration_view.xml',
+        'views/resPartner_view.xml',
+        'views/demo1_view.xml',
+        'views/demo2_view.xml',
+        'views/user_view.xml',
+
+    ]
+
+}
